27b842905c87f3f15903fb84175dae0224d4cd19
mengliang <13767029108@163.com>
2023-10-11 20:09:01 +0800

    远程代码沙箱

--- springboot-init-master/src/main/java/com/xlei/cyoj3/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,5 +1,10 @@
 package com.xlei.cyoj3.judge.codesandbox.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.xlei.cyoj3.common.ErrorCode;
+import com.xlei.cyoj3.exception.BusinessException;
 import com.xlei.cyoj3.judge.codesandbox.CodeSandbox;
 import com.xlei.cyoj3.judge.codesandbox.model.ExecuteCodeRequest;
 import com.xlei.cyoj3.judge.codesandbox.model.ExecuteCodeResponse;
@@ -10,7 +15,16 @@ import com.xlei.cyoj3.judge.codesandbox.model.ExecuteCodeResponse;
 public class RemoteCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
-        System.out.println("远程代码沙箱");
-        return null;
+        System.out.println("==============================远程代码沙箱启动==============================");
+        String url = "http://localhost:8090/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);//把请求参数转化为JSON格式使用
+        String responseStr = HttpUtil.createPost(url)
+                .body(json)
+                .execute()
+                .body();
+        if (StrUtil.isBlank(responseStr)) {
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error,message =" + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);//把字符串转化为需要的响应类
     }
 }
