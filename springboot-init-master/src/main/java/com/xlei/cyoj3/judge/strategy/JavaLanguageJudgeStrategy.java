5910a5288b0685f83f8b0a10c1581be057d38833
mengliang <13767029108@163.com>
2023-10-14 13:35:36 +0800

    当获取到的运行内存和时间为空时默认设置为0

--- springboot-init-master/src/main/java/com/xlei/cyoj3/judge/strategy/JavaLanguageJudgeStrategy.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/judge/strategy/JavaLanguageJudgeStrategy.java
@@ -8,6 +8,7 @@ import com.xlei.cyoj3.model.entity.Question;
 import com.xlei.cyoj3.model.enums.JudgeInfoMessageEnum;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Java 程序判题策略
@@ -21,8 +22,8 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy{
     @Override
     public JudgeInfo doJudge(JudgeContext judgeContext) {
         JudgeInfo judgeInfo = judgeContext.getJudgeInfo();
-        Long memory = judgeInfo.getMemory();
-        Long time = judgeInfo.getTime();
+        Long memory = Optional.ofNullable(judgeInfo.getMemory()).orElse(0L);
+        Long time = Optional.ofNullable(judgeInfo.getTime()).orElse(0L);
         List<String> inputList = judgeContext.getInputList();
         List<String> outputList = judgeContext.getOutputList();
         Question question = judgeContext.getQuestion();
