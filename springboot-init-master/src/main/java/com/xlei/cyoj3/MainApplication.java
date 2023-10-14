ef0a28f10e71bf457f19e4adc387e23e44bb886d
mengliang <13767029108@163.com>
2023-10-14 13:37:55 +0800

    增加启动标识,方便阅读

--- springboot-init-master/src/main/java/com/xlei/cyoj3/MainApplication.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/MainApplication.java
@@ -21,6 +21,7 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+        System.out.println("===========项目启动成功===========");
     }
 
 }
