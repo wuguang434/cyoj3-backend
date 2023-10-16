58916bbe419ac2f20363a5ca7904d50276f54ed3
mengliang <13767029108@163.com>
2023-10-16 12:35:02 +0800

    合并已提交页面的方法

--- springboot-init-master/src/main/java/com/xlei/cyoj3/controller/QuestionController.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/controller/QuestionController.java
@@ -11,11 +11,16 @@ import com.xlei.cyoj3.constant.UserConstant;
 import com.xlei.cyoj3.exception.BusinessException;
 import com.xlei.cyoj3.exception.ThrowUtils;
 import com.xlei.cyoj3.model.dto.question.*;
+import com.xlei.cyoj3.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.xlei.cyoj3.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.xlei.cyoj3.model.dto.user.UserQueryRequest;
 import com.xlei.cyoj3.model.entity.Question;
+import com.xlei.cyoj3.model.entity.QuestionSubmit;
 import com.xlei.cyoj3.model.entity.User;
+import com.xlei.cyoj3.model.vo.QuestionSubmitVO;
 import com.xlei.cyoj3.model.vo.QuestionVO;
 import com.xlei.cyoj3.service.QuestionService;
+import com.xlei.cyoj3.service.QuestionSubmitService;
 import com.xlei.cyoj3.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -281,4 +286,45 @@ public class QuestionController {
         return ResultUtils.success(result);
     }
 
+    @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    /**
+     * 提交题目
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return 提交记录的 id
+     */
+    @PostMapping("/question_submit/do")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+
+    /**
+     * 分页获取题目提交列表（除了管理员外，普通用户只能看到非答案、提交代码等公开信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/question_submit/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                         HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        // 从数据库中查询原始的题目提交分页信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getLoginUser(request);
+        // 返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
 }
