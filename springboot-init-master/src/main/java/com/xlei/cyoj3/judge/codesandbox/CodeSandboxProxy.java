3439439cec3ac79cebdfd64d86f9137ca7a1d9f2
mengliang <13767029108@163.com>
2023-10-20 21:22:20 +0800

    代理代码沙箱接口

--- springboot-init-master/src/main/java/com/xlei/cyoj3/judge/codesandbox/CodeSandboxProxy.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/judge/codesandbox/CodeSandboxProxy.java
@@ -5,11 +5,19 @@ import com.xlei.cyoj3.judge.codesandbox.model.ExecuteCodeResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 代理代码沙箱接口
+ */
 @Slf4j
 @AllArgsConstructor
 public class CodeSandboxProxy implements CodeSandbox {
     private final CodeSandbox codeSandbox;
 
+    /**
+     * 代码沙箱执行接口
+     * @param executeCodeRequest
+     * @return
+     */
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         log.info("\n\n代码沙箱请求信息:" + executeCodeRequest.toString());
