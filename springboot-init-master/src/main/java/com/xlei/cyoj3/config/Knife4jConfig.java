49e6dd42f2c03affab0621b7bada37f289454f0c
mengliang <13767029108@163.com>
2023-10-20 21:23:56 +0800

    自定义接口文档信息

--- springboot-init-master/src/main/java/com/xlei/cyoj3/config/Knife4jConfig.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/config/Knife4jConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,15 +26,30 @@ public class Knife4jConfig {
     @Bean
     public Docket defaultApi2() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        .title("接口文档")
-                        .description("cyoj3-backend")
-                        .version("1.0")
-                        .build())
+                .apiInfo(apiInfo())
                 .select()
                 // 指定 Controller 扫描包路径
                 .apis(RequestHandlerSelectors.basePackage("com.xlei.cyoj3.controller"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 自定义接口文档信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 接口文档的标题
+                .title("在线做题系统后端接口")
+                // 接口文档的描述信息
+                .description("用户可以选择题目，在线做题，编写代码并且提交代码；系统会对用户提交的代码，根据出题人设置的答案，来判断用户的提交结果是否正确。\n")
+                // 提供服务的是谁？可以填写你自己的地址因为是你自己提供的服务
+                .termsOfServiceUrl("https://github.com/wuguang434/cyoj3-backend")
+                .contact(new Contact("xlei", "https://github.com/wuguang434/cyoj3-backend", "xionglei213@163.com"))
+                // 版本
+                .version("1.0")
+                // 构建
                 .build();
     }
 }
