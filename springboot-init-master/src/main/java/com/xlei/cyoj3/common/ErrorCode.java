e81a249e7d43aadf94373d96b8215b2a22977a7e
mengliang <13767029108@163.com>
2023-10-22 14:01:19 +0800

    增加为空时的错误码

--- springboot-init-master/src/main/java/com/xlei/cyoj3/common/ErrorCode.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/common/ErrorCode.java
@@ -8,6 +8,7 @@ package com.xlei.cyoj3.common;
 public enum ErrorCode {
 
     SUCCESS(0, "ok"),
+    ERROR_NULL(40001,"请求数据为空"),
     PARAMS_ERROR(40000, "请求参数错误"),
     NOT_LOGIN_ERROR(40100, "未登录"),
     NO_AUTH_ERROR(40101, "无权限"),
