80c1efeb29620873c6af173fd308324bf207275c
mengliang <13767029108@163.com>
2023-10-22 14:00:40 +0800

    用户

--- springboot-init-master/src/main/java/com/xlei/cyoj3/model/entity/User.java
+++ springboot-init-master/src/main/java/com/xlei/cyoj3/model/entity/User.java
@@ -34,15 +34,7 @@ public class User implements Serializable {
      */
     private String userPassword;
 
-    /**
-     * 开放平台id
-     */
-    private String unionId;
 
-    /**
-     * 公众号openId
-     */
-    private String mpOpenId;
 
     /**
      * 用户昵称
